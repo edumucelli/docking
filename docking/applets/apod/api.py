42fc88deddfe84cd88f1a6f5cfab3789073aa698
eduardom <edumucelli@gmail.com>
2026-05-17 10:58:18 +0200

    Add GPL header to docking Python files

--- docking/applets/apod/api.py
+++ docking/applets/apod/api.py
@@ -1,3 +1,16 @@
+# Author: Eduardo Mucelli Rezende Oliveira
+# E-mail: edumucelli@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 """NASA APOD fetcher using only the Python standard library.
 
 No API key required beyond the public ``DEMO_KEY`` identifier, which api.nasa.gov
